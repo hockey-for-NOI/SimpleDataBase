4a7d9ce4cf51915c238381c3618013259817c410
2015011299 <hq15@mails.tsinghua.edu.cn>
2017-11-06 22:03:13 +0800

    IndexManager: Getobj test passed. (#9)

--- include/indexman.h
+++ include/indexman.h
@@ -22,12 +22,21 @@ public:
 	template <typename T>
 	RecordPos ins(int fileID, T const& obj) {insert(fileID, &obj);}
 	int	remove(int fileID, RecordPos const& key);
-	void*	getptr(int fileID, RecordPos const& key);
+	void	getobj(int fileID, RecordPos const& key, std::function<void(void*)> const& grabber);
+	template <typename T>
+	std::vector<T>	get(int fileID, RecordPos const& key, std::function<T(void*)> const& grabber)
+	{
+		std::vector<T> res(0);
+		getobj(fileID, key, [&res, &grabber](void* p){res.emplace_back(grabber(p));});
+		return res;
+	}
 
 protected:
 	bool _leafBinaryInsert(int fileID, int pageID, void const* objptr);
 	int	_innerRemove(int fileID, int pageID, int depth, RecordPos const& pos);
 	int	_leafRemove(int fileID, int pageID, RecordPos const& pos);
+	void _innerGetobj(int fileID, int pageID, int depth, RecordPos const& pos, std::function<void(void*)> const& grabber);
+	void _leafGetobj(int fileID, int pageID, RecordPos const& pos, std::function<void(void*)> const& grabber);
 };
 
 }	// end namespace SimpleDataBase
