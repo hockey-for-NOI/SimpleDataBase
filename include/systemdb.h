9e1be842a029ac72932669f816029ff3a08b3588
2015011299 <hq15@mails.tsinghua.edu.cn>
2017-12-16 14:32:47 +0800

    Add DB management. (#13)

--- include/systemdb.h
+++ include/systemdb.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "indexman.h"
+#include <string>
+#include <vector>
 
 namespace	SimpleDataBase
 {
@@ -8,15 +10,28 @@ class	SystemDB
 {
 
 public:
+	static	const	std::string	DEFAULT_ROOT_DIR;
+	static	const	std::string SYSTEM_DB_NAME;
+
 	SystemDB&	get_instance();
 	inline	std::shared_ptr<RecordManager>	getRecordManager() {return recordManager;}
 	inline	std::shared_ptr<IndexManager>	getIndexManager() {return indexManager;}
+
+	inline	std::string	getCurrentDir() {return DEFAULT_ROOT_DIR + "/" + current_db;}
+	inline	std::string getTableFile(std::string table) {return getCurrentDir() + "/" + table;}
+
+	bool	createDB(std::string const& name);
+	bool	dropDB(std::string const& name);
+	std::string	showDB();
+	bool	useDB(std::string const& name);
 
 private:
 	SystemDB();
 
 	std::shared_ptr<RecordManager> recordManager;
 	std::shared_ptr<IndexManager> indexManager;
+
+	std::string current_db;
 
 	static	SystemDB*	m_instance;
 };
--- src/systemdb.cpp
+++ src/systemdb.cpp
@@ -1,7 +1,13 @@
 #include "systemdb.h"
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
 
 namespace	SimpleDataBase
 {
+
+const std::string SystemDB::DEFAULT_ROOT_DIR = "/data/db";
+const std::string SystemDB::SYSTEM_DB_NAME = "system";
 
 SystemDB*	SystemDB::m_instance = nullptr;
 
@@ -17,6 +23,76 @@ SystemDB::SystemDB()
 	auto bfm = std::make_shared<BufPageManager>(fm);
 	recordManager = new RecordManager(fm, bfm);
 	indexManager = new IndexManager(fm, bfm);
+
+	current_db = SYSTEM_DB_NAME;
+	FILE *f = fopen(getCurrentDir().c_str(), "a");
+	if (f)
+	{
+		fclose(f);
+		system(("rm " + getCurrentDir()).c_str());
+		system(("mkdir -p " + getCurrentDir()).c_str());
+	}
+	current_db = "";
+}
+
+bool	SystemDB::createDB(std::string const& name)
+{
+	if (name == SYSTEM_DB_NAME || !name.length()) return 0;
+	std::string prev_db = current_db;
+	current_db = SYSTEM_DB_NAME;
+
+	File *f = fopen(getTableFile(name).c_str(), "r");
+	if (f) {fclose(f); return 0;}
+
+	recordManager->createFile(getTableFile(name));
+	current_db = name;
+	system(("mkdir -p " + getCurrentDir()).c_str());
+	
+	current_db = prev_db;
+	return 1;
+}
+
+bool	SystemDB::dropDB(std::string const& name)
+{
+	if (name == SYSTEM_DB_NAME || !name.length()) return 0;
+	std::string prev_db = current_db;
+	current_db = SYSTEM_DB_NAME;
+
+	File *f = fopen(getTableFile(name).c_str(), "r");
+	if (!f) return 0;
+	fclose(f);
+	system(("rm " + getTableFile(name)).c_str());
+	current_db = name;
+	system(("rm -rf " + getCurrentDir()).c_str());
+
+	current_db = prev_db == name ? "" : prev_db;
+	return 1;
+}
+
+std::string SystemDB::showDB()
+{
+	static	const	std::string tmpfile = "/tmp/mydb.showdb.tmp.0001";
+
+	std::string prev_db = current_db;
+	current_db = SYSTEM_DB_NAME;
+	system(("ls " + getCurrentDir() + " > " + tmpfile).c_str());
+	current_db = prev_db;
+
+	std::ifstream f(tmpfile);
+	return std::string(std::istreambuf_iterator<char>(f), std::istreambuf_iterator<char>());
+}
+
+bool	SystemDB::useDB(std::string name)
+{
+	if (name == SYSTEM_DB_NAME || !name.length()) return 0;
+	std::string prev_db = current_db;
+	current_db = SYSTEM_DB_NAME;
+
+	File *f = fopen(getTableFile(name).c_str(), "r");
+	if (!f) return 0;
+	fclose(f);
+	current_db = name;
+	return 1;
 }
 
 }	// end namespace SimpleDataBase
