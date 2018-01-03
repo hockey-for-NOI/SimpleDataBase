ea32b265638540d8c8ef458c65f3696e54a43d0d
2015011299 <hq15@mails.tsinghua.edu.cn>
2018-01-03 13:58:57 +0800

    Refractor translateCond.

--- src/translate.cpp
+++ src/translate.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 #include <iostream>
 
+using	namespace	std;
+
 namespace	SimpleDataBase
 {
 
@@ -180,341 +182,85 @@ std::shared_ptr<std::function<bool(void const*)> >	translateCond(hsql::Expr cons
 		case hsql::Expr::LESS_EQ:
 		case hsql::Expr::GREATER_EQ:
 		{
-			if (expr.expr->type == hsql::kExprColumnRef && expr.expr2->type == hsql::kExprColumnRef)
-				return nullptr;
-			if (expr.expr->type != hsql::kExprColumnRef && expr.expr2->type != hsql::kExprColumnRef)
-				return nullptr;
-			auto p = expr.expr, q = expr.expr2;
-			if (expr.expr->type != hsql::kExprColumnRef) p = expr.expr2, q = expr.expr;
-			std::string name = p->name;
-			if (!area.count(name)) return nullptr;
-			auto const& area1 = area[name];
-			unsigned short o1 = area1.offset;
-			unsigned short l1 = area1.len;
-			if (q->type == hsql::kExprLiteralNull)
+			if (!(expr.expr || expr.expr2)) return nullptr;
+			auto pli = translateInt(*expr.expr, area);
+			auto pri = translateInt(*expr.expr2, area);
+			if (pli && pri)
 			{
-				return std::make_shared<std::function<bool(void const*)> >([](void const* ptr) {
-					return false;
-				});
-			}
-			if (expr.op_type == hsql::Expr::SIMPLE_OP && expr.op_char == '=')
-			{
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralInt)
+				auto li = *pli, ri = *pri;
+				if (expr.op_type == hsql::Expr::SIMPLE_OP && expr.op_char == '=')
 				{
-					int tmp = q->ival;
-					if (tmp != q->ival) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v == tmp;
+					return std::make_shared<std::function<bool(void const*)> >([li, ri](void const* ptr) {
+						auto x = li(ptr);
+						auto y = ri(ptr);
+						return x && y ? *x == *y : false;
 					});
 				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralString)
+				if (expr.op_type == hsql::Expr::SIMPLE_OP && expr.op_char == '<')
 				{
-					std::string tmp = q->name;
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) == 0;
+					return std::make_shared<std::function<bool(void const*)> >([li, ri](void const* ptr) {
+						auto x = li(ptr);
+						auto y = ri(ptr);
+						return x && y ? *x < *y : false;
 					});
 				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralInt)
+				if (expr.op_type == hsql::Expr::SIMPLE_OP && expr.op_char == '>')
 				{
-					std::string tmp = std::to_string(q->ival);
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) == 0;
+					return std::make_shared<std::function<bool(void const*)> >([li, ri](void const* ptr) {
+						auto x = li(ptr);
+						auto y = ri(ptr);
+						return x && y ? *x > *y : false;
 					});
 				}
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralString)
+				if (expr.op_type == hsql::Expr::NOT_EQUALS)
 				{
-					int tmp = std::stoi(q->name);
-					if (std::to_string(tmp) != q->name) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v == tmp;
+					return std::make_shared<std::function<bool(void const*)> >([li, ri](void const* ptr) {
+						auto x = li(ptr);
+						auto y = ri(ptr);
+						return x && y ? *x != *y : false;
+					});
+				}
+				if (expr.op_type == hsql::Expr::LESS_EQ)
+				{
+					return std::make_shared<std::function<bool(void const*)> >([li, ri](void const* ptr) {
+						auto x = li(ptr);
+						auto y = ri(ptr);
+						return x && y ? *x <= *y : false;
+					});
+				}
+				if (expr.op_type == hsql::Expr::GREATER_EQ)
+				{
+					return std::make_shared<std::function<bool(void const*)> >([li, ri](void const* ptr) {
+						auto x = li(ptr);
+						auto y = ri(ptr);
+						return x && y ? *x >= *y : false;
 					});
 				}
 			}
-			if (expr.op_type == hsql::Expr::SIMPLE_OP && expr.op_char == '<')
+			auto pls = translateString(*expr.expr, area);
+			auto prs = translateString(*expr.expr2, area);
+			if (pls && prs)
 			{
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralInt)
+				auto ls = *pls;
+				auto rs = *prs;
+				if (expr.op_type == hsql::Expr::SIMPLE_OP && expr.op_char == '=')
 				{
-					int tmp = q->ival;
-					if (tmp != q->ival) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v < tmp;
+					return std::make_shared<std::function<bool(void const*)> >([ls, rs](void const* ptr) {
+						auto x = ls(ptr);
+						auto y = rs(ptr);
+						return x && y ? *x == *y : false;
 					});
 				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralString)
+				if (expr.op_type == hsql::Expr::NOT_EQUALS)
 				{
-					std::string tmp = q->name;
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) < 0;
-					});
-				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralInt)
-				{
-					std::string tmp = std::to_string(q->ival);
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) < 0;
-					});
-				}
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralString)
-				{
-					int tmp = std::stoi(q->name);
-					if (std::to_string(tmp) != q->name) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v < tmp;
+					return std::make_shared<std::function<bool(void const*)> >([ls, rs](void const* ptr) {
+						auto x = ls(ptr);
+						auto y = rs(ptr);
+						return x && y ? *x != *y : false;
 					});
 				}
 			}
-			if (expr.op_type == hsql::Expr::SIMPLE_OP && expr.op_char == '>')
-			{
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralInt)
-				{
-					int tmp = q->ival;
-					if (tmp != q->ival) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v > tmp;
-					});
-				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralString)
-				{
-					std::string tmp = q->name;
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) > 0;
-					});
-				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralInt)
-				{
-					std::string tmp = std::to_string(q->ival);
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) > 0;
-					});
-				}
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralString)
-				{
-					int tmp = std::stoi(q->name);
-					if (std::to_string(tmp) != q->name) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v > tmp;
-					});
-				}
-			}
-			if (expr.op_type == hsql::Expr::NOT_EQUALS)
-			{
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralInt)
-				{
-					int tmp = q->ival;
-					if (tmp != q->ival) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v != tmp;
-					});
-				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralString)
-				{
-					std::string tmp = q->name;
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) != 0;
-					});
-				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralInt)
-				{
-					std::string tmp = std::to_string(q->ival);
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) != 0;
-					});
-				}
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralString)
-				{
-					int tmp = std::stoi(q->name);
-					if (std::to_string(tmp) != q->name) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v != tmp;
-					});
-				}
-			}
-			if (expr.op_type == hsql::Expr::LESS_EQ)
-			{
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralInt)
-				{
-					int tmp = q->ival;
-					if (tmp != q->ival) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v <= tmp;
-					});
-				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralString)
-				{
-					std::string tmp = q->name;
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) <= 0;
-					});
-				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralInt)
-				{
-					std::string tmp = std::to_string(q->ival);
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) <= 0;
-					});
-				}
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralString)
-				{
-					int tmp = std::stoi(q->name);
-					if (std::to_string(tmp) != q->name) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v <= tmp;
-					});
-				}
-			}
-			if (expr.op_type == hsql::Expr::GREATER_EQ)
-			{
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralInt)
-				{
-					int tmp = q->ival;
-					if (tmp != q->ival) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v >= tmp;
-					});
-				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralString)
-				{
-					std::string tmp = q->name;
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) >= 0;
-					});
-				}
-				if (area1.type == Area::VARCHAR_T && q->type == hsql::kExprLiteralInt)
-				{
-					std::string tmp = std::to_string(q->ival);
-					if (l1 < tmp.size()) l1 = tmp.size();
-					return std::make_shared<std::function<bool(void const*)> >([o1, l1, tmp](void const* ptr) {
-						char buf1[l1], buf2[l1];
-						unsigned char* t = (unsigned char*) ptr;
-						memset(buf1, 0, sizeof(buf1));
-						memset(buf2, 0, sizeof(buf2));
-						memcpy(buf1, t + o1 + 1, sizeof(buf1));
-						memcpy(buf2, tmp.c_str(), tmp.length());
-						return (!t[o1]) && memcmp(buf1, buf2, l1) >= 0;
-					});
-				}
-				if (area1.type == Area::INT_T && q->type == hsql::kExprLiteralString)
-				{
-					int tmp = std::stoi(q->name);
-					if (std::to_string(tmp) != q->name) return nullptr;
-					return std::make_shared<std::function<bool(void const*)> >([o1, tmp](void const* ptr) {
-						int v;
-						unsigned char* t = (unsigned char*) ptr;
-						memcpy(&v, t + o1 + 1, 4);
-						return (!t[o1]) && v >= tmp;
-					});
-				}
-			}
+			return nullptr;
 		}
 		break;
 	}
